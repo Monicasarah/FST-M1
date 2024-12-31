904b10f260c56fad319662153b8014c7487be6fb
Monicasarah <mgprabha@in.ibm.com>
2024-12-31 11:26:41 +0530

    Add files via upload

--- Java/Activities/Activity8.java
+++ Java/Activities/Activity8.java
@@ -1,25 +1,40 @@
-package Activity8;
+package activities;
+
+class CustomException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String message = null;
+
+    public CustomException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
 
 public class Activity8 {
-	 public static void exceptionTest(String value) throws CustomException {
-	        if (value == null) {
-	            throw new CustomException("Input string is null. This is a custom exception.");
-	        } else {
-	            System.out.println("Input string: " + value);
-	        }
-	    }
+    public static void main(String[] a){
+        try {
+            // Method call with correct input
+            Activity8.exceptionTest("Will print to console");
+            // Method call with incorrect input
+            Activity8.exceptionTest(null); // Exception is thrown here
+            Activity8.exceptionTest("Won't execute");
+        } catch(CustomException mae) {
+            System.out.println("Inside catch block: " + mae.getMessage());
+        }
+    }
 
-	    public static void main(String[] args) {
-	        try {
-	            // Test with a non-null string
-	            exceptionTest("Hello, World!");
-
-	            // Test with a null value
-	            exceptionTest(null);
-	        } catch (CustomException e) {
-	            // Print custom error message
-	            System.out.println("Caught exception: " + e.getMessage());
-	        }
-	    }
-	}
-
+    static void exceptionTest(String str) throws CustomException {
+        if(str == null) {
+            throw new CustomException("String value is null");
+        } else {
+            System.out.println(str);
+        }
+    }
+}
